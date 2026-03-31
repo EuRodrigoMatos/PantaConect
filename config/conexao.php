ff121494cca3f4ffe7b42886ed2657d5e0958b55
EuRodrigoMatos <mattos.anjos.rodrigo@gmail.com>
2026-03-31 11:31:40 -0400

    Adicionando segurança

--- config/conexao.php
+++ config/conexao.php
@@ -1,15 +1,32 @@
 <?php
-$host = "172.18.0.242";
-$port = "5432";
-$db   = "pantaconect";
-$user = "webservpg";
-$pass = "Pantanal04480";
+
+function carregarEnv($caminho) {
+    if (!file_exists($caminho)) return;
+
+    $linhas = file($caminho, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+    foreach ($linhas as $linha) {
+        if (strpos(trim($linha), '#') === 0) continue;
+
+        list($nome, $valor) = explode('=', $linha, 2);
+        $_ENV[$nome] = $valor;
+    }
+}
+
+// carrega o .env
+carregarEnv(__DIR__ . '/../.env');
+
+// pega variáveis
+$host = $_ENV['DB_HOST'];
+$port = $_ENV['DB_PORT'];
+$db   = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
 
 try {
     $conn = new PDO("pgsql:host=$host;port=$port;dbname=$db", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Define o schema
     $conn->exec("SET search_path TO pantaconect");
 
 } catch (PDOException $e) {
